feef33fabb92fc850afa888aee23f925a48fdbaf
keita_eguchi <35883400+eggchi21@users.noreply.github.com>
2021-08-31 19:04:10 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const handsfree = new Handsfree({
+    showDebug: true,
+    weboji: {
+        enabled: true,
+    },
+
+})
+handsfree.start();
+handsfree.plugin.facePointer.enable();
+
+// handsfree.plugin.facePointer.pointer.speed = {
+//     x: 0.1,
+//     y: 0.1
+// }
+// handsfree.plugin.facePointer.pointer.offset = {
+//     x: 1000,
+//     y: 600
+// }
+
+document.addEventListener('handsfree-data', (event) => {
+    const data = event.detail
+
+    if (paddleWidth / 2 > data.weboji.pointer.x) {
+        paddleX = 0;
+    } else if (canvas.width - data.weboji.pointer.x < paddleWidth / 2) {
+        paddleX = canvas.width - paddleWidth;
+    } else {
+        paddleX = data.weboji.pointer.x - paddleWidth / 2;
+    }
+})
